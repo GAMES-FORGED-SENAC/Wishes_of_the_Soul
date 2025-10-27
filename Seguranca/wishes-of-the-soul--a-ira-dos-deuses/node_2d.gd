cf9a8ef775e817713603db025661cc96e32ea465
Nadine Helen <max.rf0513@gmail.com>
2025-10-27 09:11:31 -0300

    Ajuste de projeto

---
+++ Seguranca/wishes-of-the-soul--a-ira-dos-deuses/node_2d.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var contador = 0
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://fase1.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://credits.tscn")
+
+
+func _on_voltarcreditos_pressed() -> void:
+	get_tree().change_scene_to_file("res://node_2d.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()
